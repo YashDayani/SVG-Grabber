e9c746591202cd5855be9ffac2e6007a3d378f0c
Yash Dayani <52958495+YashDayani@users.noreply.github.com>
2024-05-19 09:39:17 +0530

    19-May-24

--- js/svg_display.js
+++ js/svg_display.js
@@ -1,34 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//   const svgContainer = document.getElementById('svg-container');
-//   const totalSVG = document.getElementById('totalSVG');
-//   const svgResult = document.getElementById('svgResult');
-
-//   // Retrieve the fetched SVGs from the service worker
-//   chrome.runtime.sendMessage({ action: 'getSVGs' }, (response) => {
-//     const svgArray = response.svgArray;
-
-//     if (svgArray && svgArray.length > 0) {
-//       svgArray.forEach((svgString) => {
-//         const svgElement = new DOMParser().parseFromString(svgString, 'image/svg+xml').documentElement;
-//         svgContainer.innerHTML += `
-//           <div class="card">
-//             <div class="svg_place">${svgElement.outerHTML}</div>
-//             <div class="btn_place">
-//               <button class="svg_copy">Copy</button>
-//               <button class="svg_download">Download</button>
-//             </div>
-//           </div>
-//         `;
-//         totalSVG.textContent=svgArray.length;
-//       });
-//     } else {
-//       svgResult.textContent = 'No SVGs fetched. 😣';
-//     }
-//   });
-// });
-
-
-
 document.addEventListener('DOMContentLoaded', function() {
   const svgContainer = document.getElementById('svg-container');
   const totalSVG = document.getElementById('totalSVG');
