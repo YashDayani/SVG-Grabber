51c13bfe810ca6e9d1d64ae3cfc40d29d8d7422d
Yash Dayani <52958495+YashDayani@users.noreply.github.com>
2024-06-23 09:51:03 +0530

    23-June-24

--- js/interact.js
+++ js/interact.js
@@ -1,46 +1,38 @@
-// svg_display_script.js
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const svgContainer = document.getElementById('svg-container');
 
-    // Add click event listener to each download button
-    svgContainer.addEventListener('click', function(event) {
+
+    svgContainer.addEventListener('click', function (event) {
         if (event.target.classList.contains('svg_download')) {
-            // Get the SVG content
+
             const svgContent = event.target.parentElement.previousElementSibling.innerHTML;
-
-            // Create a blob from the SVG content
             const blob = new Blob([svgContent], { type: 'image/svg+xml' });
-
-            // Create a temporary anchor element
             const a = document.createElement('a');
-            a.href = URL.createObjectURL(blob);
-            a.download = 'image.svg'; // Set the filename for the downloaded file
 
-            // Simulate click on the anchor element to trigger download
+            a.href = URL.createObjectURL(blob);
+            a.download = 'image.svg';
             a.click();
 
-            // Clean up
             URL.revokeObjectURL(a.href);
         }
     });
 });
 
 
-// svg_display_script.js
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const svgContainer = document.getElementById('svg-container');
     const clipboard = new ClipboardJS('.svg_copy', {
-        text: function(trigger) {
+        text: function (trigger) {
             return trigger.parentElement.previousElementSibling.innerHTML;
         }
     });
 
-    clipboard.on('success', function(event) {
+    clipboard.on('success', function (event) {
         alert('SVG copied to clipboard!');
         event.clearSelection();
     });
 
-    clipboard.on('error', function(event) {
+    clipboard.on('error', function (event) {
         console.error('Failed to copy SVG:', event.error);
     });
 });
--- js/popup.js
+++ js/popup.js
@@ -12,10 +12,8 @@ document.addEventListener('DOMContentLoaded', function () {
       const svgArray = svgElements[0].result;
       console.log('Fetched SVGs:', svgArray);
 
-      // Store the fetched SVGs in the service worker
       chrome.runtime.sendMessage({ action: 'storeSVGs', svgArray });
 
-      // Redirect to svg_display.html
       chrome.tabs.create({ url: chrome.runtime.getURL('/html/svg_display.html') });
     } catch (err) {
       console.log(err, "Error in popup script");
@@ -26,20 +24,17 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 function fetchSVGs() {
-  const svgSet = new Set(); // Set to store unique SVG strings
+  const svgSet = new Set();
 
-  // Function to fetch SVG content from an image URL
   function fetchSVGFromURL(url) {
     return fetch(url)
       .then(response => response.text())
       .catch(error => {
         console.error('Error fetching SVG:', error);
-        return null; // Return null in case of error
+        return null;
       });
   }
 
-  // Function to clean SVG element attributes
-  // Function to clean SVG element attributes
 function cleanSVG(svgElement) {
   svgElement.removeAttribute('height');
   svgElement.removeAttribute('width');
@@ -47,12 +42,10 @@ function cleanSVG(svgElement) {
   svgElement.removeAttribute('id');
   svgElement.removeAttribute('class');
 
-  // Define the xlink namespace if it's missing on the SVG element
   if (!svgElement.hasAttributeNS('http://www.w3.org/2000/xmlns/', 'xlink') && !svgElement.hasAttribute('xmlns:xlink')) {
     svgElement.setAttributeNS('http://www.w3.org/2000/xmlns/', 'xmlns:xlink', 'http://www.w3.org/2000/svg');
   }
 
-  // Define the xlink namespace for any <use> elements with xlink:href attribute
   const useElements = svgElement.querySelectorAll('use');
   useElements.forEach(useElement => {
     if (!useElement.hasAttributeNS('http://www.w3.org/2000/xmlns/', 'xlink') && !useElement.getAttribute('xmlns:xlink')) {
@@ -60,49 +53,41 @@ function cleanSVG(svgElement) {
     }
   });
 
-  // Set the xmlns attribute to "http://www.w3.org/2000/svg" if it's missing
   if (!svgElement.hasAttribute('xmlns') || svgElement.getAttribute('xmlns') !== 'http://www.w3.org/2000/svg') {
     svgElement.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
   }
 }
 
-// Iterate over each SVG element on the page
 Array.from(document.querySelectorAll('svg')).forEach(svg => {
   const svgClone = svg.cloneNode(true);
-  cleanSVG(svgClone); // Clean SVG element attributes
+  cleanSVG(svgClone);
 
   const svgString = svgClone.outerHTML;
 
-  // Check if the SVG string is not empty and doesn't contain <parsererror>, or other unwanted elements
   if (svgString.trim().length > 0 && !svgString.includes('href="/')
 
   ) {
-    svgSet.add(svgString); // Add the SVG string to the Set
+    svgSet.add(svgString);
   }
 });
 
 
-  // Iterate over each SVG element on the page
   Array.from(document.querySelectorAll('svg')).forEach(svg => {
     const svgClone = svg.cloneNode(true);
-    cleanSVG(svgClone); // Clean SVG element attributes
+    cleanSVG(svgClone);
 
     const svgString = svgClone.outerHTML;
 
-    // Check if the SVG string is not empty and doesn't contain <parsererror>, or other unwanted elements
     if (svgString.trim().length > 0 && !svgString.includes('href="/')) {
-      svgSet.add(svgString); // Add the SVG string to the Set
+      svgSet.add(svgString);
     }
   });
 
-  // Array to store promises for fetching SVG content from img URLs
   const imgPromises = [];
 
-  // Iterate over each <img> element on the page
   Array.from(document.querySelectorAll('img')).forEach(img => {
     const src = img.getAttribute('src');
 
-    // Check if the image source ends with .svg
     if (src && src.endsWith('.svg')) {
       imgPromises.push(
         fetchSVGFromURL(src)
@@ -111,18 +96,16 @@ Array.from(document.querySelectorAll('svg')).forEach(svg => {
               const parser = new DOMParser();
               const doc = parser.parseFromString(svgContent, 'image/svg+xml');
               const svgElement = doc.documentElement;
-              cleanSVG(svgElement); // Clean SVG element attributes
-              svgSet.add(svgElement.outerHTML); // Add the SVG content to the Set
+              cleanSVG(svgElement);
+              svgSet.add(svgElement.outerHTML);
             }
           })
       );
     }
   });
 
-  // Wait for all promises to resolve using Promise.all()
   return Promise.all(imgPromises)
     .then(() => {
-      // Convert the Set to an array and return
       const svgArray = Array.from(svgSet);
       return svgArray;
     });
